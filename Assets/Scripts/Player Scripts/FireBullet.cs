06053cc7522c008fc05e4bb22f6c554e7fa3e2df
yemrehalis <yemrehalis@gmail.com>
2020-09-30 20:59:22 +0300

    PlayerSHoot Script added

---
+++ Assets/Scripts/Player Scripts/FireBullet.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    private float speed = 10f;
+    private Animator anim;
+    private bool canMove;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Start()
+    {
+        canMove = true;
+        StartCoroutine(DisableBullet(5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    void Move()
+    {
+        if (canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime;
+            transform.position = temp;
+        }
+    }
+
+
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+
+        set
+        {
+            speed = value;
+
+        }
+    }
+
+    IEnumerator DisableBullet(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == MyTags.BEETLE_TAG || collision.gameObject.tag == MyTags.SNAIL_TAG)
+        {
+            anim.Play("Explode");
+            canMove = false;
+            StartCoroutine(DisableBullet(.1f));
+        }
+    }
+}
